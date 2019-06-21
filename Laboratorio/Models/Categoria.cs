a188e0657ce856dde66d43b3eb8934e2c130cb4d
dilancastillo <dilancastillom@gmail.com>
2019-06-21 08:54:01 -0500

    Initial commit

---
+++ Laboratorio/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [Required (ErrorMessage = "Campo Obligatorio")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe de tener de 3 a 50 caracteres")]
+        public string Nombre { get; set; }
+        [StringLength(256)]
+        public string Descripcion { get; set; }
+        public Boolean Estado { get; set; } = true;
+
+        public ICollection<Curso> Cursos { get; set; }
+    }
+}
